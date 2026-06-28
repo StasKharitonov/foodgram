56ff26a040fb31618c0f1d09e6e4f838a69d35f4
StasKharitonov <youngstarmoscow@gmail.com>
2026-06-29 01:52:00 +0300

    to review

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,5 +1,4 @@
-from djoser.serializers import (TokenCreateSerializer, UserCreateSerializer,
-                                UserSerializer)
+from djoser.serializers import TokenCreateSerializer, UserCreateSerializer
 from rest_framework import serializers
 from rest_framework.authtoken.models import Token
 
