cae5592f5cfc78eae89b50b01bd902936588c64f
StasKharitonov <youngstamsocw@gmail.com>
2026-07-05 13:29:08 +0300

    5th commit

--- backend/api/pagination.py
+++ backend/api/pagination.py
@@ -1,9 +1,7 @@
-from rest_framework.pagination import (
-    PageNumberPagination as DRFPageNumberPagination
-)
+from rest_framework import pagination
 
 
-class PageNumberPagination(DRFPageNumberPagination):
+class PageNumberPagination(pagination.PageNumberPagination):
     page_size = 6
     page_size_query_param = 'limit'
     max_page_size = 100
